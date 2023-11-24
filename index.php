9aadc1c58f4ddf0685c3350f486b2b177a0775f3
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-24 16:38:32 +0100

    ADD- axios and vue

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PhP To Do List</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 <body>
     <header>
